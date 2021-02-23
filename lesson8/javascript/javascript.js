aca7c255a62d53984a70a192454d6788a7fc4d34
malhermon <her17033@byui.edu>
2021-02-23 15:56:57 -0600

    commit

---
+++ lesson8/javascript/javascript.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+    document.getElementsByClassName("nav-bar")[0].classList.toggle("menu-id");
+  }
+  
+  const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var d = new Date();
+  var day = d.getDay() 
+  var date = d.getDate();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
+  document.getElementById("currentDate").innerHTML = dateStr;
+  document.getElementById("currentYear").innerHTML = year;
+  
+  let imagesToLoad = document.querySelectorAll('img[data-src]');
+  const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+  };
+
+  function adjustSeverity(rating) {
+    document.getElementById("rating-value").innerHTML = rating;
+}
