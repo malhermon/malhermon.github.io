9560ac270b45f3f4ceceee5112e6495b94534b63
malhermon <her17033@byui.edu>
2021-03-15 20:52:23 -0500

    commit

--- lesson11/javascript/preston.js
+++ lesson11/javascript/preston.js
@@ -1,33 +1,32 @@
-
 // Menu
 function toggleMenu() {
-   document.getElementsByClassName("nav-bar")[0].classList.toggle("menu-id");
- }
+  document.getElementsByClassName("nav-bar")[0].classList.toggle("menu-id");
+}
 
- // Date and year
- const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
- const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
- var d = new Date();
- var day = d.getDay() 
- var date = d.getDate();
- var month = d.getMonth();
- var year = d.getFullYear();
- var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
- document.getElementById("currentDate").innerHTML = dateStr;
- document.getElementById("currentYear").innerHTML = year;
+// Date and year
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay() 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
+document.getElementById("currentDate").innerHTML = dateStr;
+document.getElementById("currentYear").innerHTML = year;
 
 // Pancake Announcement
 if (day1[day] == 'Friday') {
-   document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-   document.getElementById("pancakes").style.display = "block";
- }
- else {
-   document.getElementById("pancakes").style.display = "none";
- }
+  document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+  document.getElementById("pancakes").style.display = "block";
+}
+else {
+  document.getElementById("pancakes").style.display = "none";
+}
 
-// <script src="javascript/javascript.js"></script> Weather for Weather Summary
+// Weather Summary
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1";
-
+ 
 fetch(apiURL)
  .then((response) => response.json())
  .then((town) => {
@@ -77,84 +76,24 @@ let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (
   }
 document.getElementById('windchill').innerHTML = windchill;
 
-// Gallery 
-
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-  const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-      image.removeAttribute('data-src');
-    };
-  };
-  
-  const imgOptions = {
-    threshold: 0.5,
-    rootMargin: "0px 0px -100px 0px"
-  };
-  
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    }, imgOptions);
-  
-  
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
-
-  // Storm Center
-  function adjustSeverity(rating) {
-    document.getElementById("rating-value").innerHTML = rating;
-}
-
-// Homepage
+// Events
 
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 fetch(requestURL)
-.then(function (response) {
+ .then(function (response) {
    return response.json();
-})
-.then(function (jsonObject) {
-   //console.table(jsonObject);  // temporary checking for valid response and data parsing
+ })
+ .then(function (jsonObject) {
    const towns = jsonObject['towns'];
-   towns.sort((a,b) => (a.averageRainfall > b.averageRainfall) ? 1 : ((b.averageRainfall > a.averageRainfall) ? -1 : 0));
-   towns.reverse();
-   for (let i = 0; i < towns.length; i++) {
-      if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
-      let card = document.createElement('section');
-      let name = document.createElement('h2');
-      let motto = document.createElement('h4');
-      let image = document.createElement('img');
-      let yearFounded = document.createElement('p');
-      let currentPopulation = document.createElement('p');
-      let averageRainfall = document.createElement('p');
-      let details = document.createElement('div');
-      name.textContent = towns[i].name;
-      motto.textContent = towns[i].motto;
-      currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-      averageRainfall.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + ' ' + 'in.';
-      yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-      image.setAttribute('src', "towns/" + towns[i].photo);
-      image.setAttribute('alt', towns[i].name);
-      details.setAttribute('id', 'details');
-      card.appendChild(details);
-      card.appendChild(image);
-      details.appendChild(name);
-      details.appendChild(motto);
-      details.appendChild(yearFounded);
-      details.appendChild(currentPopulation);
-      details.appendChild(averageRainfall);
-      document.querySelector('div.cards').appendChild(card);
-      }
+   for (let i = 0; i < towns.length; i++ ) {
+       if (towns[i].name == 'Preston') {
+        let events = towns[i].events;
+        for (let i=0; i < events.length; i++) {
+        let event = document.createElement('p');
+        event.innerHTML = events[i];
+        document.querySelector('.town-events').appendChild(event);
+        }
+    }
    }
 });
