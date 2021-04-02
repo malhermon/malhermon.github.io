6df86d669796c7d521420d9031efbeccaa20ec70
malhermon <her17033@byui.edu>
2021-04-02 10:18:13 -0500

    commit

--- final/javascript/directory.js
+++ final/javascript/directory.js
@@ -1,14 +1,53 @@
 
 // Directory Javascript
 
-const url = "https://malhermon.github.io/final/file.json";
-fetch(url)
-  .then((response) => response.json())
-    .then((data) => {
-        data.directory.forEach((directory, index) => {
-            document.getElementsByClassName('business1' + (index + 1)).innerHTML = directory.name;
-            document.getElementsByClassName('business2' + (index + 1)).innerHTML = directory.address;
-            document.getElementsByClassName('business3' + (index + 1)).innerHTML = directory.member;
-            document.getElementsByClassName('business4' + (index + 1)).innerHTML = directory.phone;
-        });
+const jsonURL = 'https://malhermon.github.io/final/file.json';
+
+fetch(jsonURL)
+ 
+.then(function(response) {
+  return response.json();
+})
+
+.then(function (jsonObject) {
+  console.table(jsonObject);
+
+  const business = jsonObject['business'];
+
+  for(let i = 0; i < business.name; i++){
+    let business = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let address = document.createElement('address');
+    let member = document.createElement('div');
+    let phone = document.createElement('div');
+    let website = document.createElement('link');
+    let logo = document.createElement('img');
+
+    h2.textContent = directory [i].name + '' + business[i].name;
+
+    address.setAttribute('class','address')
+    address.textContent = 'Address: ' + business[i].address;
+
+    member.setAttribute('class','member')
+    address.textContent = 'Member: ' + business[i].member;
+
+    phone.setAttribute('class','phone')
+    phone.textContent = 'Phone: ' + business[i].phone;
+
+    website.setAttribute('class','website')
+    website.textContent = 'Website: ' + business[i].website;
+
+    logo.setAttribute('src', directory[i].logo);
+    logo.setAttribute('alt', directory[i].name+''+business[i].name);
+
+    business.appendChild(h2);
+    business.appendChild(address);
+    business.appendChild(member);
+    business.appendChild(phone);
+    business.appendChild(website);
+    business.appendChild(logo);
+
+    document.querySelector('div.business').appendChild(business);
+
+  }
 });
